d2ef02884a1cf29293d7b59f8c96c834573b8deb
Izar de Villasante <idevillasante@carrerasresearch.org>
2022-04-19 19:22:58 +0200

    sysdata

--- data-raw/sysdata.R
+++ data-raw/sysdata.R
@@ -1,13 +1,9 @@
 source("./data-raw/anno.R")
 control<-readRDS("/data/SCNA/control.rds")
 control<-control[1:25,]
-<<<<<<< HEAD
 library("RFpurify")
 RFpurify_ABSOLUTE<-RFpurify::RFpurify_ABSOLUTE
 library(ChAMP)
 data(hm450.manifest.hg19)
 
 usethis::use_data(anno,control,RFpurify_ABSOLUTE,hm450.manifest.hg19, overwrite = TRUE,internal = TRUE)
-=======
-usethis::use_data(anno,control, overwrite = TRUE,internal = TRUE)
->>>>>>> package
