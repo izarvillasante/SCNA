d98b2372be31160a42ffe91642ba2f72c6bd5140
Izar-de-Villasante <idevillasante@carrerasresearch.org>
2022-04-20 10:44:53 +0200

    finished run_conumee

--- R/run_conumee.R
+++ R/run_conumee.R
@@ -16,7 +16,7 @@
 #' default = "log2"
 #' @param Sample_Name Samples to be analysed. If is NULL all columns in input
 #' file will be used. Accepts numbered index and names. Default=NULL
-#' @param probeid
+#' @param probeid name of column with probe ids.
 #' @return Log2r intensities and segment files.
 #' @export
 #'
@@ -24,7 +24,8 @@
 #'
 #' #data("anno")
 #' #data("controls")
-#' library(conumee)
+#' #library(conumee)
+#' library(SummarizedExperiment)
 #' intensity<-readRDS("./analysis/ChAMP/intensities.rds")
 #' intensity<-conumee::CNV.load(intensity)@intensity
 #' run_conumee(intensities=intensity)
@@ -34,7 +35,7 @@
 run_conumee<-function(intensities, anno_file=NULL, ctrl_file='WB', Sample_Name=NULL,
                       seg.folder = "Segments", log2r.folder = "log2",
                       out="analysis/CONUMEE/", probeid="probeid"){
-
+  requireNamespace("conumee")
   if(!is.null(anno_file ))anno <- readRDS(anno_file)
   message("anno")
 
@@ -43,45 +44,80 @@ run_conumee<-function(intensities, anno_file=NULL, ctrl_file='WB', Sample_Name=N
   if(is.null(intensities) ){
     stop("Provide conumee with either a valid path to file or a data.frame with intensities",call. = F)
   }
+  dir.create(out,recursive=TRUE,showWarnings=FALSE)
   intensities<- read_intensity(infile = intensities,Sample_Name=Sample_Name,probeid=probeid)
-
+  data.table::setkey(intensities,probeid)
   message("intensities")
-  log2_file<-paste(out,log2r.folder,"_log2.txt", sep="")
-  message(log2_file)
 
-  cg1<-intersect(intensities$probeid,rownames(control@intensity))
-  message("cg1 ok")
+  # Intersect common probes
+  cg1<-intersect(intensities[[probeid]],rownames(control@intensity))
   cgcommon<-intersect(cg1,names(anno@probes@ranges))
-  intensities <- intensities[intensities$probeid %in% cgcommon,]
+  message(head(cgcommon))
+  # Subset
+  intensities<-intensities[cgcommon,]
   control@intensity <-control@intensity[cgcommon,]
   anno@probes<-anno@probes[names(anno@probes)%in% cgcommon,]
+  # conumee:
 
-  intensities<-as.data.frame(intensities)
-  message(intensities[1,])
-  # rownames(intensities)<-intensities$probeid
-  # names(intensities)[2]<-"intensity"
+  idx<-intensities[[probeid]]
+  intensities[,(probeid):=NULL]
+  # Cluster:
+  ncores<-parallel::detectCores()-2
+  if (!is.na(as.numeric(Sys.getenv("SLURM_CPUS_PER_TASK"))))ncores=as.numeric(Sys.getenv("SLURM_CPUS_PER_TASK"))
+  cl<- parallel::makePSOCKcluster(ncores, outfile="")
+  doParallel::registerDoParallel(cl)
 
+  res<-foreach::foreach(i=1:NCOL(intensities),#isplitIndices(1400,chunks=ncores),
+                        .combine='c',
+                        .multicombine = F,
+                        .inorder=F,
+                        .errorhandling = "pass"
+  )%dopar%{
+    Sample_Name <- names(intensities)[i]
+    my.datacnv<-conumee::CNV.load(intensities[,..i])
+    rownames(my.datacnv@intensity)<-idx
+    fit  <- conumee::CNV.fit(my.datacnv, control, anno)
+    #Log2 <- list(log2ratio=fit@fit$ratio, Purity=ss$Purity_Impute_RFPurify.Absolute.[ss$Sample_Name%in%Sample_Name])
+    fit2 <- conumee::CNV.segment(conumee::CNV.detail(conumee::CNV.bin(fit)))
+    write.table(conumee::CNV.write(fit2, what = "segments"),sprintf("%sSegments_%s.txt",out,Sample_Name),col.names = T, row.names = F, quote = F, sep = "\t")
+    message("Sgements file" ," saved")
+    log2ratio<-as.data.frame(fit@fit$ratio)
+    names(log2ratio) <- Sample_Name
+    #log2ratio$purity <- ss[ss$Sample_Name== Sample_Name,Purity_Impute_RFPurity.Absolute.]
 
-
-
-  my.datacnv<-conumee::CNV.load(input= intensities[,2])
-  rownames(my.datacnv@intensity)<-intensities$probeid
-  fit  <- conumee::CNV.fit(my.datacnv, control, anno)
-  #Log2 <- list(log2ratio=fit@fit$ratio, Purity=ss$Purity_Impute_RFPurify.Absolute.[ss$Sample_Name%in%Sample_Name])
-  fit2 <- conumee::CNV.segment(conumee::CNV.detail(conumee::CNV.bin(fit)))
-  write.table(conumee::CNV.write(fit2, what = "segments"),sprintf("%sSegments_%s.txt",out,Sample_Name),col.names = T, row.names = F, quote = F, sep = "\t")
-  message("Sgements file" ," saved")
-  log2ratio<-as.data.frame(fit@fit$ratio)
-  names(log2ratio) <- Sample_Name
-  #log2ratio$purity <- ss[ss$Sample_Name== Sample_Name,Purity_Impute_RFPurity.Absolute.]
-  write.table(log2ratio,log2_file)
-  message(log2_file ," saved")
-  return(log2ratio)
-  #}else return(NULL)
-}
+    log2_file<-paste(out,log2r.folder,"/",Sample_Name,"_log2.txt", sep="")
+    dir.create(dirname(log2_file),recursive = TRUE,showWarnings=FALSE)
+    message(log2_file)
+    write.table(log2ratio,log2_file)
+    message(log2_file ," saved")
+    return(log2ratio)
+  }
+  parallel::stopCluster(cl)
+return(res)
+  }
+#
+#   # rownames(intensities)<-intensities$probeid
+#   # names(intensities)[2]<-"intensity"
+#
+#   my.datacnv<-conumee::CNV.load(input= intensities[,2])
+#   rownames(my.datacnv@intensity)<-intensities$probeid
+#   fit  <- conumee::CNV.fit(my.datacnv, control, anno)
+#   #Log2 <- list(log2ratio=fit@fit$ratio, Purity=ss$Purity_Impute_RFPurify.Absolute.[ss$Sample_Name%in%Sample_Name])
+#   fit2 <- conumee::CNV.segment(conumee::CNV.detail(conumee::CNV.bin(fit)))
+#   write.table(conumee::CNV.write(fit2, what = "segments"),sprintf("%sSegments_%s.txt",out,Sample_Name),col.names = T, row.names = F, quote = F, sep = "\t")
+#   message("Sgements file" ," saved")
+#   log2ratio<-as.data.frame(fit@fit$ratio)
+#   names(log2ratio) <- Sample_Name
+#   #log2ratio$purity <- ss[ss$Sample_Name== Sample_Name,Purity_Impute_RFPurity.Absolute.]
+#   write.table(log2ratio,log2_file)
+#   message(log2_file ," saved")
+#   return(log2ratio)
+#   #}else return(NULL)
+# }
 
 #' @export
 read_intensity<-function(infile,Sample_Name=NULL,probeid="probeid"){
+  requireNamespace("data.table")
 if(!is.character(probeid)&length(probeid)==1){
   stop("probeid must be the name of a single column. ")
 }
@@ -129,7 +165,6 @@ tryCatch(
       }, error = function(e) "invalid file"
   )
   return(my.data)
+
 }
-
-
 
--- data-raw/sysdata.R
+++ data-raw/sysdata.R
@@ -1,4 +1,9 @@
 source("./data-raw/anno.R")
 control<-readRDS("/data/SCNA/control.rds")
 control<-control[1:25,]
-usethis::use_data(anno,control, overwrite = TRUE,internal = TRUE)
+library("RFpurify")
+RFpurify_ABSOLUTE<-RFpurify::RFpurify_ABSOLUTE
+library(ChAMP)
+data(hm450.manifest.hg19)
+
+usethis::use_data(anno,control,RFpurify_ABSOLUTE,hm450.manifest.hg19, overwrite = TRUE,internal = TRUE)
